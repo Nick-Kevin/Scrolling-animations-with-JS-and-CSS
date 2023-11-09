b64649498cf9e74d8963e5f5cc48ff686f25af8f
Nick-Kevin <nicblou23@gmail.com>
2023-11-09 18:24:43 +0000

    update sapp.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 function reveal() {
 	var reveals = document.querySelectorAll(".reveal");
+
+	for(var i = 0; i < reveals[i].length; i++) {
+		var windowHeight = window.innerHeight;
+		var revealDistanceFromTop = reveals[i].getBoundingClientRect().top;
+		var heightToRevealTheElement = 150;
+	}
 }
