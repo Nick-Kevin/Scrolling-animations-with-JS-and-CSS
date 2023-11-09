135a68446059a946029809dc5934f97846b9ee97
Nick-Kevin <nicblou23@gmail.com>
2023-11-09 18:54:13 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*alert("coucou");
+
 function reveal() {
 	var reveals = document.querySelectorAll(".reveal");
 
@@ -16,4 +18,26 @@ function reveal() {
 
 window.addEventListener('scroll', reveal);
 
-reveals();
+reveal();*/
+
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for 
+
+(var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
